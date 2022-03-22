5830385e6425811a11fac3fcc2673f55c8de2973
bui <thibault@crowdsec.net>
2022-03-22 13:00:58 +0100

    upgrade lib and update files

--- crowdsec-php-bouncer.php
+++ crowdsec-php-bouncer.php
@@ -3,17 +3,21 @@
 require_once __DIR__.'/vendor/autoload.php';
 require_once __DIR__.'/settings.php';
 
-use CrowdSecBouncer\StandAloneBounce;
+use CrowdSecBouncer\StandaloneBounce;
 
-$bounce = new StandAloneBounce();
+$bounce = new StandaloneBounce();
+
 // Retro compatibility with crowdsec php lib < 0.14.0
 if(isset($crowdSecStandaloneBouncerConfig['bouncing_level']) && $crowdSecStandaloneBouncerConfig['bouncing_level'] === 'normal_boucing'){
     $crowdSecStandaloneBouncerConfig['bouncing_level'] = 'normal_bouncing';
 }elseif($crowdSecStandaloneBouncerConfig['bouncing_level'] === 'flex_boucing'){
     $crowdSecStandaloneBouncerConfig['bouncing_level'] = 'flex_bouncing';
 }
-
 $bounce->setDebug($crowdSecStandaloneBouncerConfig['debug_mode']??false);
 $bounce->setDisplayErrors($crowdSecStandaloneBouncerConfig['display_errors']??false);
 $bounce->init($crowdSecStandaloneBouncerConfig);
-$bounce->safelyBounce();
+
+/** @var $crowdSecStandaloneBouncerConfig */
+$bounce->safelyBounce($crowdSecStandaloneBouncerConfig);
+
+?>
