79b522a3ff121ac547e0197942e325b21063ec51
bui <thibault@crowdsec.net>
2022-04-04 13:50:02 +0200

    fix bouncer code ?

--- crowdsec-php-bouncer-refresh.php
+++ crowdsec-php-bouncer-refresh.php
@@ -5,7 +5,7 @@ require_once __DIR__.'/settings.php';
 
 use CrowdSecBouncer\StandAloneBounce;
 
-$bounce = new StandAloneBounce();
+$bouncer = new StandAloneBounce();
 // Retro compatibility with crowdsec php lib < 0.14.0
 if(isset($crowdSecStandaloneBouncerConfig['bouncing_level']) && $crowdSecStandaloneBouncerConfig['bouncing_level'] === 'normal_boucing'){
     $crowdSecStandaloneBouncerConfig['bouncing_level'] = 'normal_bouncing';
@@ -13,6 +13,6 @@ if(isset($crowdSecStandaloneBouncerConfig['bouncing_level']) && $crowdSecStandal
     $crowdSecStandaloneBouncerConfig['bouncing_level'] = 'flex_bouncing';
 }
 
-$bouncer = $bounce->init($crowdSecStandaloneBouncerConfig);
-$bouncer->refreshBlocklistCache();
+$bounce = $bouncer->init($crowdSecStandaloneBouncerConfig);
+$bounce->refreshBlocklistCache();
 echo 'OK';
--- crowdsec-php-bouncer.php
+++ crowdsec-php-bouncer.php
@@ -5,7 +5,7 @@ require_once __DIR__.'/settings.php';
 
 use CrowdSecBouncer\StandaloneBounce;
 
-$bounce = new StandaloneBounce();
+$bouncer = new StandaloneBounce();
 
 // Retro compatibility with crowdsec php lib < 0.14.0
 if(isset($crowdSecStandaloneBouncerConfig['bouncing_level']) && $crowdSecStandaloneBouncerConfig['bouncing_level'] === 'normal_boucing'){
@@ -13,6 +13,8 @@ if(isset($crowdSecStandaloneBouncerConfig['bouncing_level']) && $crowdSecStandal
 }elseif($crowdSecStandaloneBouncerConfig['bouncing_level'] === 'flex_boucing'){
     $crowdSecStandaloneBouncerConfig['bouncing_level'] = 'flex_bouncing';
 }
+
+$bounce = $bouncer->init($crowdSecStandaloneBouncerConfig);
 
 /** @var $crowdSecStandaloneBouncerConfig */
 $bounce->safelyBounce($crowdSecStandaloneBouncerConfig);
